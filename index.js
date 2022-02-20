75dad6a17ed35fbc2bbea11200ed6ac7233b5a09
michaelferreir12345678 <michaelferreiradesousa@gmail.com>
2022-02-20 15:07:52 -0300

    Instalando EJS, configurações de views e separando rotas

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express'); 
-
+const pizzasRoutes = require("./routes/pizzas");
+const path = require('path');
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Chegou aqui')
-})
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(pizzasRoutes);
 
 app.listen(5000, () => console.log('Servidor em execução'));
