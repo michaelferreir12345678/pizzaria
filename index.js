47fbee6d3fe370db0b2618b8a221d9d5543981c5
michaelferreir12345678 <michaelferreiradesousa@gmail.com>
2022-03-13 13:49:52 -0300

    inclusão da view lista-edição e início de tentativa do method DELETE juntamente com a aula do Leo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const methodOverride = require('method-override');
 
 const baseRoutes = require('./routes/index');
 const pizzasRoutes = require('./routes/pizzas');
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use(baseRoutes);
 app.use('/pizzas', pizzasRoutes);
